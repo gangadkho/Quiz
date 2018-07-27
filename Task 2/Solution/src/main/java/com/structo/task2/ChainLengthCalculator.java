ad2395db6750f6a85e7c6272e108eea06f8435b4
Gangadharan Khoteeswarun <gangadkho@yahoo.com>
2018-07-27 21:36:17 +0800

    ChainLengthCalculator updated

--- Task 2/Solution/src/main/java/com/structo/task2/ChainLengthCalculator.java
+++ Task 2/Solution/src/main/java/com/structo/task2/ChainLengthCalculator.java
@@ -1,13 +1,57 @@
 package com.structo.task2;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Maximum chain length calculator!
  *
  */
-public class ChainLengthCalculator 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Chain Length Calculator!" );
+public class ChainLengthCalculator {
+
+    public static void main( String[] args ) {
+        int i = 100; //Initializing i and j with default values
+        int j = 200;
+        Scanner scanner = null;
+        
+        System.out.println( "Chain Length Calculator" );
+        System.out.println("Please enter the range of integers i and j:");
+        
+        try {
+            scanner = new Scanner(System.in);
+            i = scanner.nextInt();
+            j = scanner.nextInt();
+        } catch (NoSuchElementException | IllegalStateException ex) {
+            System.out.println("Exception occurred " + ex);
+            System.out.println("Taking default values for i and j");
+        } finally {
+            scanner.close();
+        }
+        
+        int maxSoFar = 0;
+        
+        for(int x = i; x <= j; x++) {
+            int chainLength = calculateChainLength(x, 0);
+            if(chainLength > maxSoFar) {
+                maxSoFar = chainLength;
+            }
+        }
+        System.out.println("************** The maximum chain length for the given range is **************");
+        System.out.println(maxSoFar);
+        
     }
+    
+    private static int calculateChainLength(int n, int count) {
+        count++;
+        if (n != 1) {
+            if (n % 2 != 0) {
+                n = 3 * n + 1;
+            } else {
+                n = n / 2;
+            }
+            count = calculateChainLength(n, count);
+        }
+        return count;
+    }
+
 }
