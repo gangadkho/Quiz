21288c66bc75173e12d5f7fb592ec585a7ebb66d
gangadkho <gangadkho@yahoo.com>
2018-07-27 17:31:18 +0800

    Added socket emit methods for the requests from client

--- Task 1/index.js
+++ Task 1/index.js
@@ -6,11 +6,11 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('time-now', function (data) {
   	console.log('Time now should be given in time-response event name');
-    
+    socket.emit('time-response', 'Time Response is ***********');
   });
 
   socket.on('date-tomorrow', function (data) {
   	console.log('Tomorrows date should be replied in tomorrow-response event name');
-   
+   	socket.emit('tomorrow-response', 'Tomorrow Response is ***********');
   });
 });
