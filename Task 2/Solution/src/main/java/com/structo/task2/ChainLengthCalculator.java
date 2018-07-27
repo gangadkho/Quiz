b47e655750edc1cbad0589c5123822dc830065ac
Gangadharan Khoteeswarun <gangadkho@yahoo.com>
2018-07-27 21:09:11 +0800

    Project Structure for Task 2

---
+++ Task 2/Solution/src/main/java/com/structo/task2/ChainLengthCalculator.java
@@ -0,0 +1,13 @@
+package com.structo.task2;
+
+/**
+ * Hello world!
+ *
+ */
+public class ChainLengthCalculator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Chain Length Calculator!" );
+    }
+}
