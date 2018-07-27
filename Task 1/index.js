d00d09b238a3d4f91b9c5f5101eb326a99c8f270
gangadkho <gangadkho@yahoo.com>
2018-07-27 17:22:26 +0800

    Skeleton Code of SocketIO Server

--- Task 1/index.js
+++ Task 1/index.js
@@ -1,10 +1,16 @@
 var io = require("socket.io").listen(8100);
-console.log("Server started at 8100");
-io.sockets.on("connection",function(socket){
+console.log("Server started at 8100 port");
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('message', 'You are connected!');
+
+  socket.on('time-now', function (data) {
+  	console.log('Time now should be given in time-response event name');
     
-    console.log("Server-Client Connected!");
-    socket.on("message",function(data){
-        console.log("Message from client is");
-        console.log(data);
-    });
+  });
+
+  socket.on('date-tomorrow', function (data) {
+  	console.log('Tomorrows date should be replied in tomorrow-response event name');
+   
+  });
 });
